48531dd3774a570f40d076c501b070a396b282f6
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-01-10 16:00:54 -0700

    31. Adding the Header Component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,40 @@
 <template>
+
   <div>
+
+    <app-the-header @sidenavToggle="displaySidenav = !displaySidenav">
+    </app-the-header>
+
+    <app-the-sidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false"
+    >
+    </app-the-sidenav>
+
     <Nuxt />
   </div>
+
 </template>
+
+<script>
+import TheHeader from '../components/navigation/TheHeader';
+import TheSidenav from '../components/navigation/TheSidenav';
+
+export default {
+
+  components: {
+    'app-the-header': TheHeader,
+    'app-the-sidenav': TheSidenav
+  },
+
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
+
+}
+</script>
 
 <style>
 html {
--- pages/index.vue
+++ pages/index.vue
@@ -12,21 +12,21 @@
 
     <div class="featured-posts">
       <app-post-preview
-        :id="1"
+        id="1"
         title="Hello there!"
         previewText="This is my first post"
         thumbnail="https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png"
       >
       </app-post-preview>
       <app-post-preview
-        :id="2"
+        id="2"
         title="Sonsayo!"
         previewText="This is my second post"
         thumbnail="https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png"
       >
       </app-post-preview>
       <app-post-preview
-        :id="3"
+        id="3"
         title="Hello man!"
         previewText="This is my third post"
         thumbnail="https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png"
