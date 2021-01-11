949e3e2d7c7864f2bbcbc5cd51bb035b55c4d202
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-01-11 11:04:25 -0700

    40. Adding Dynamic Data

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -2,34 +2,18 @@
 
   <section class="post-list">
 
-      <app-post-preview
-        id="1"
-        :isAdmin="isAdmin"
-        title="Hello there!"
-        previewText="This is my first post"
-        thumbnail="https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png"
-      >
-      </app-post-preview>
+    <app-post-preview
+      v-for="post in posts"
+      :key="post.id"
+      :id="post.id"
+      :isAdmin="isAdmin"
+      :thumbnail="post.thumbnail"
+      :title="post.title"
+      :previewText="post.previewText"
+    >
+    </app-post-preview>
 
-      <app-post-preview
-        id="2"
-        :isAdmin="isAdmin"
-        title="Sonsayo!"
-        previewText="This is my second post"
-        thumbnail="https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png"
-      >
-      </app-post-preview>
-
-      <app-post-preview
-        id="3"
-        :isAdmin="isAdmin"
-        title="Hello man!"
-        previewText="This is my third post"
-        thumbnail="https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png"
-      >
-      </app-post-preview>
-
-    </section>
+  </section>
 
 </template>
 
@@ -43,10 +27,17 @@ export default {
   },
 
   props: {
+
     isAdmin: {
       type: Boolean,
       default: false
+    },
+
+    posts: {
+      type: Array,
+      required: true
     }
+
   }
 
 }
--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,8 @@
 
     </section>
 
-    <app-post-list></app-post-list>
+    <app-post-list :posts="loadedPosts">
+    </app-post-list>
 
   </div>
 
@@ -20,9 +21,30 @@
 import PostList from '../components/posts/PostList';
 
 export default {
+
   components: {
     'app-post-list': PostList
+  },
+
+  data() {
+    return {
+      loadedPosts: [
+        {
+          id: '1',
+          title: 'First post',
+          previewText: 'Super cool first post!',
+          thumbnail: 'https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png'
+        },
+        {
+          id: '2',
+          title: 'Second post',
+          previewText: 'Super cool second post!',
+          thumbnail: 'https://prod-discovery.edx-cdn.org/media/course/image/efc25613-f0ea-4423-bfcd-4d94c317f085-5dd84e82e22b.small.png'
+        }
+      ]
+    }
   }
+
 }
 </script>
 
